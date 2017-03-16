a5a6a1f39da313b3bf563160c5b19fd4f0ce7a19
ftrombotto <franco.trombotto@mercadolibre.com>
2017-03-16 13:40:52 -0300

    Integration test del api controller

---
+++ test/integration/com/meli/academy/grails/test/APIControllerIntegrationSpec.groovy
@@ -0,0 +1,15 @@
+package com.meli.academy.grails.test
+
+import grails.test.spock.IntegrationSpec
+
+class APIControllerIntegrationSpec extends IntegrationSpec {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+    }
+}
